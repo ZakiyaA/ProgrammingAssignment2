dbdeed7d3a26b7bd9a620d21e6fb582d7e3ee5de
ZakiyaAlfughi <47117848+ZakiyaA@users.noreply.github.com>
2021-12-28 18:28:11 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,31 +8,32 @@ setwd('C:/Users/rubind1/Documents/Coursera-R')
 ## use random values between 0-100 to fill the matrix
 ## I simply set the input x as a matrixx
 ## and then set the solved value "s" as a null
-## then I changed every reference to "mean" to "solve"
+
 makeCacheMatrix <- function(x = matrix(floor(runif(16, min=0, max=101)),4,4)) {
-  s <- NULL
-  set <- function(y) {
+  j <- NULL
+  set <- function(y){
     x <<- y
-    s <<- NULL
+    j <<- NULL
   }
-  get <- function() x
-  setInverse <- function(solve) s <<- Inverse
-  getInverse <- function() s
-  list(set = set, get = get,
-       setInverse = setInverse,
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
        getInverse = getInverse)
 }
-##
-## Same here, changed "mean" to "Inverse" and "m" to "s"
-cacheInverse <- function(x, ...) {
-  s <- x$getInverse()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
+
+## then I changed every reference to "mean" to "solve"
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setInverse(s)
-  s
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
 
